82582be53abfa1e453ec4d3dfa22d62459acd4d8
Christian Unigarro <cunigarrod@gmail.com>
2022-11-27 14:36:57 -0500

    Test added for Menu Form Service.

--- RestaurantAppCUD/RestaurantAppTests/ClientFormServiceTest.cs
+++ RestaurantAppCUD/RestaurantAppTests/ClientFormServiceTest.cs
@@ -2,12 +2,5 @@ namespace RestaurantAppTests
 {
     public class ClientFormServiceTest
     {
-
-        [Test]
-        public void Test1()
-        {
-
-            Assert.Pass();
-        }
     }
 }
--- RestaurantAppCUD/RestaurantAppTests/MenuFormServiceTest.cs
+++ RestaurantAppCUD/RestaurantAppTests/MenuFormServiceTest.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppLogicCUD.Services;
 
 namespace RestaurantAppTests
 {
     internal class MenuFormServiceTest
     {
         [Test]
-        public void Test1()
+        public void CalculateCost()
         {
-            MenuFormService.calculateCost
-            Assert.Pass();
+            List<Int32> values = new List<Int32> { 1, 2, 3 };
+
+            Int32 result = MenuFormService.calculateCost(values);
+
+            Assert.AreEqual(6, result);
         }
     }
 }
